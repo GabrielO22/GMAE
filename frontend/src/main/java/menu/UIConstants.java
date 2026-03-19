eb3030d5e2ee316c8c4233c117b3442a4d34e052
Dante Luong <dantel1@uci.edu>
2026-03-19 01:00:01 -0700

    Clean up UIConstants

--- frontend/src/main/java/menu/UIConstants.java
+++ frontend/src/main/java/menu/UIConstants.java
@@ -9,12 +9,12 @@ public class UIConstants {
     public static final String OFF_WHITE         = "#EEEEEE";
     public static final String CARD_GREY         = "#CCCCCC";
     public static final String RELIC_SLOT_DARK   = "#888888";
-    public static final String RELIC_SLOT_LIGHT  = "#AAAAAA"; // NEW: checkerboard alt
+    public static final String RELIC_SLOT_LIGHT  = "#AAAAAA";
     public static final String WHITE             = "#FFFFFF";
-    public static final String BLUE_BORDER       = "#4A90D9"; // NEW: main card border
-    public static final String TAB_SELECTED      = "#333333"; // NEW: active tab
-    public static final String TAB_UNSELECTED    = "#999999"; // NEW: inactive tab
-    public static final String BTN_HOVER         = "#777777"; // NEW: hover state
+    public static final String BLUE_BORDER       = "#4A90D9";
+    public static final String TAB_SELECTED      = "#333333";
+    public static final String TAB_UNSELECTED    = "#999999";
+    public static final String BTN_HOVER         = "#777777";
 
     // Fonts & Styles
     public static final String FONT_FAMILY = "'Press Start 2P'";
